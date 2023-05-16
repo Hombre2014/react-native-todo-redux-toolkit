3f29fc15c7d73052a041b662c1b50fd8ee1ec6b6
Yuriy Chamkoriyski <chamkoriyski@gmail.com>
2023-05-16 11:09:34 +0200

    Implement ToDo list

--- App.js
+++ App.js
@@ -1,11 +1,13 @@
 import { SafeAreaView, View } from 'react-native';
 import ToDoHeader from './src/components/ToDoHeader';
+import ToDoList from './src/components/ToDoList';
 
 export default function App() {
   const RootApp = () => {
     return (
       <SafeAreaView>
         <ToDoHeader />
+        <ToDoList />
       </SafeAreaView>
     )
   }
